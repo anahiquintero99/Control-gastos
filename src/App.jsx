cc99ea09b5da66634dd99ed2a34b81ad86f12ce9
Anahi Quintero <anitaquintero99@gmail.com>
2022-12-26 20:18:55 -0600

    New project Control-gastos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <h1>Control de Gastos</h1>
+      <Header />
+    </div>
+  );
+}
+export default App;
