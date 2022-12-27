15dd548d0c0746fe879cc3002fbbcc641f285c6a
Anahi Quintero <anitaquintero99@gmail.com>
2022-12-26 22:34:52 -0600

    presupuesto validate messsage

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+
   return (
     <div>
-      <h1>Control de Gastos</h1>
-      <Header />
+      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
     </div>
   );
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto valido");
+      return;
+    }
+
+    setMensaje("");
+
+    console.log("Presupuesto valido");
+  };
+
   return (
-    <div>
-      <h2>Nuevo Presupuesto</h2>
+    <div className="contenerdor-presupuesto contenedor sombra">
+      <form onSubmit={handlePresupuesto} className="formulario">
+        <div className="campo">
+          <label htmlFor="">Defnir presupuesto</label>
+
+          <input
+            className="nuevo-presupuesto"
+            type="number"
+            placeholder="Añade Presupuesto"
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
+          />
+        </div>
+
+        <input type="submit" value="Añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+      </form>
     </div>
   );
 };
